e6f095b42ef8bc6ab2ef6479160b803b47367fb1
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2017-02-04 14:26:34 +0500

    serde usage example

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,15 @@ mod server;
 
 fn main() {
 	let c = config::Config::load();
+
+	// SERDE JSON
+	{
+		let json_c = serde_json::to_string(&c).unwrap();
+		println!("c : [{}]", json_c);
+
+		let des_c: config::Config = serde_json::from_str(&json_c).unwrap();
+		println!("des_c [{:?}", des_c);
+	}
+
 	server::serve(c);
 }
